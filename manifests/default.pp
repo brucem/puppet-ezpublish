b1aacdf205fc7a1779cf6d866b3e8839e4cd17b7
Bruce Morrison <bruce.morrison@stuffandcontent.com>
2013-04-16 10:30:35 +1000

    Remove default index.html and set docroot directory to be owned by web user.

--- manifests/default.pp
+++ manifests/default.pp
@@ -4,10 +4,21 @@
 # the default docroot
 #
 class ezpublish::default inherits ezpublish {
+  include ezpublish::params
 
   # TODO: This should be a parameter
   $default_conf_file = '/etc/apache2/conf.d/ezpublish'
   $docroot = '/var/www/web'
+
+  file {'/var/www/':
+    ensure => 'directory',
+    owner  => $ezpublish::params::vhost_docroot_owner,
+    group  => $ezpublish::params::vhost_docroot_group,
+  }
+
+  file {'/var/www/index.html':
+    ensure => 'absent',
+  }
 
   file{ $default_conf_file:
     ensure  => 'present',
